4a47a3a25daa19a921d5de88fb82e1bf5b3ec933
Redan <reedaann@gmail.com>
2024-03-11 19:18:16 +0100

    Completed Loops

---
+++ Assets/ForLoops.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            if (i <= 10)
+            {
+                Debug.Log(i);
+            }
+            else if (i <= 20)
+            {
+                if (i % 2 == 0)
+                {
+                    Debug.Log(i);
+                }
+            }
+            else if (i <= 30)
+            {
+                if (i % 2 == 1)
+                {
+                    Debug.Log(i);
+                }
+            }
+            
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/SpeedLoop.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Create a program where you increment the value of speed by 5 every second -- when speed is greater 
+    //than MaxSpeed, stop incrementing (Random between 60 and 120)
+    //Hint use coroutine
+
+    public int speed;
+    public int MaxSpeed;
+
+    void Start()
+    {
+        MaxSpeed = Random.Range(60, 100);
+
+        StartCoroutine(SpeedAccelerate());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator SpeedAccelerate()
+    {
+        while (speed + 5 < MaxSpeed)
+        {
+            speed += 5;
+            yield return new WaitForSeconds(1.0f);
+        }
+        
+    }
+}
